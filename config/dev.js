592acd9c8ec688eda2ce1b6d77ab682641d93027
Than_Wa1 <thanmyintwai.my@gmail.com>
2022-10-13 19:43:57 +1000

    Configuration setup

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: 'xxxxxxxxxxxxxx',
+    googleClientSecret: 'xxxxxxxxxxxxx',
+    mongoURI: 'mongodb+srv://xxxxxxxxxxxxxxx',
+    cookieKey: 'xxxxxxxxxxxxxxxx',
+    stripePublishableKey: 'xxxxxxxxxxxxxxxxx',
+    stripeSecretKey: 'xxxxxxxxxxxxxxxxxxxxxx',
+    sendGridKey: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    redirectDomain: 'http://localhost:3000'
+}
